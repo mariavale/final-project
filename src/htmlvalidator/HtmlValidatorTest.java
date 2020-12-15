6d21ff615d325f84697e151067d98ad1ac58fbbf
Maria Valentini <mvalentini1@css.edu>
2020-12-14 21:34:51 -0600

    Test stuff

--- src/htmlvalidator/HtmlValidatorTest.java
+++ src/htmlvalidator/HtmlValidatorTest.java
@@ -15,10 +15,22 @@ public class HtmlValidatorTest {
     public static void main(String[] args) {
         // <b>Hi</b><br/>
         Queue<HtmlTag> tags = new LinkedList<HtmlTag>();
+        tags.add(new HtmlTag("html", true));
+        tags.add(new HtmlTag("head", true));
+        tags.add(new HtmlTag("title", true));
+        tags.add(new HtmlTag("head", true));
         tags.add(new HtmlTag("b", true));      // <b>
         tags.add(new HtmlTag("b", false));     // </b>
         tags.add(new HtmlTag("br"));           // <br/>
+        tags.add(new HtmlTag("title", false));
+        tags.add(new HtmlTag("head", false));
+        tags.add(new HtmlTag("html", false));
+        
+        System.out.println(tags);
 
         HtmlValidator validator = new HtmlValidator(tags);
+        
+        validator.removeAll("<head>");
+        System.out.println(tags);
     }
 }
